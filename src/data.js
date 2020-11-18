4169a027782199699895a1c58770efbed24c6f56
brianartea <bri@expertdesign.cc>
2020-11-18 18:18:25 -0500

    Added gradient to library scrollbar

---
+++ src/data.js
@@ -0,0 +1,106 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Fame Cover",
+      artist: "Brian Artea and Mike Hricko",
+      cover: "https://i1.sndcdn.com/artworks-000508917009-zdv0di-t500x500.jpg",
+      audio: "https://expertdesign.cc/songs/Fame%20(Cover)%2011-08-20.mp3",
+      color: ["#11BAD6", "#EC0591"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Dizzy Illusion",
+      artist: "Brian Artea and Mike Hricko",
+      cover: "https://i1.sndcdn.com/artworks-000508917009-zdv0di-t500x500.jpg",
+      audio:
+        "https://expertdesign.cc/songs/dizzy-illusion-New%20Mix%206-27-20.mp3",
+      color: ["#D8E36B", "#D8E36B"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Land of Misfits",
+      artist: "Brian Artea",
+      cover: "https://i1.sndcdn.com/artworks-000508917009-zdv0di-t500x500.jpg",
+      audio: "https://expertdesign.cc/songs/Land%20of%20Misfits.mp3",
+      color: ["#11BAD6", "#EC0591"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "With a Little Help From My Friends",
+      artist: "Brian Artea and Mike Hricko",
+      cover: "https://i1.sndcdn.com/artworks-000508917009-zdv0di-t500x500.jpg",
+      audio:
+        "https://expertdesign.cc/songs/with-alittle-help-from-my-friends.mp3",
+      color: ["#D8E36B", "#D8E36B"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
